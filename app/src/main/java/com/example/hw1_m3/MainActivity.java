f8b6cde5b6f9cbc56b66fb671e1f74849b323a95
NurPro06 <jakeevnurbolovich@gmail.com>
2024-11-05 15:37:31 +0600

    пкупек

--- app/src/main/java/com/example/hw1_m3/MainActivity.java
+++ app/src/main/java/com/example/hw1_m3/MainActivity.java
@@ -1,19 +1,19 @@
 package com.example.hw1_m3;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private int count = 0;
     private TextView counterTextView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         counterTextView = findViewById(R.id.text_view);
         Button incrementButton = findViewById(R.id.button_plus);
         Button sendButton = findViewById(R.id.send);
+        Button resetButton = findViewById(R.id.button_reset);
 
         incrementButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,21 +39,27 @@ public class MainActivity extends AppCompatActivity {
                 sendCountToWhatsApp(count);
             }
         });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count = 0;
+                counterTextView.setText(String.valueOf(count));
+            }
+        });
     }
 
     private void sendCountToWhatsApp(int count) {
-        String phoneNumber = "0508081889";
+        String phoneNumber = "+995508081889";
         String message = "Счетчик: " + count;
 
-        Intent sendIntent = new Intent();
-        sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, message);
-        sendIntent.setType("text/plain");
+        Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+        sendIntent.setData(Uri.parse("whatsapp://send?phone=" + phoneNumber + "&text=" + Uri.encode(message)));
 
-        sendIntent.setPackage("com.whatsapp");
         try {
             startActivity(sendIntent);
         } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this, "WhatsApp не установлен", Toast.LENGTH_SHORT).show();
         }
     }
 }
